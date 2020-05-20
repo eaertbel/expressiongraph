cd5bcf4e78ff96aa2c7218b80f9b0a7073b47215
Erwin Aertbelien <Erwin.Aertbelien@kuleuven.be>
2020-05-20 16:59:05 +0200

    added documentation for the integrator

--- include/expressiongraph/integrator.hpp
+++ include/expressiongraph/integrator.hpp
@@ -35,6 +35,20 @@ namespace KDL {
 
     using IntegrandType=std::function< void(double,double*) >;
 
+    /**
+     * An integrator object using adaptive Simpon's method[^1] with the stopping criterion of [^2].
+     * 
+     * It integrates a 3th order polynomial perfectly in a minimum amount of function calls (4)
+     *
+     * The amount of computations is 2^(D+2) function evaluations (of a function resulting in vecsize elements) with D=the depth.  You
+     * specify the minimum required depth and the maximally allowable depth.  epsilon is the required
+     * precision (of the integrated value) used as a stop criterion for the integration.  The worst case for the estimation
+     * of the precision for the vecsize elements is used as a stop criterion.  It is assumed that it is more efficient to compute
+     * the vecsize elements together. (as is the often the case with expressiongraph nodes).
+     *
+     * [1] G.F. Kuncir (1962), "Algorithm 103: Simpson's rule integrator", Communications of the ACM, 5 (6): 347, doi:10.1145/367766.368179
+     * [2]J.N. Lyness (1969), "Notes on the adaptive Simpson quadrature routine", Journal of the ACM, 16 (3): 483–495, doi:10.1145/321526.321537
+     */
     class IntegratorAdaptiveSimpson {
             double epsilon;
             int        maxRecDepth;
@@ -69,6 +83,10 @@ namespace KDL {
             double* adaptiveSimpsonsAux(IntegrandType f, double a, double b, double eps,
                                       double* whole, double* fa, double* fb, double* fm, int rec);
         public:
+
+            /**
+             * Constructs an integrator object using adaptive Simpon's method[^1] with the stopping criterion of [^2].
+             */
             IntegratorAdaptiveSimpson( double _epsilon, int _minRecDepth, int _maxRecDepth, int _vecsize);
             int get_err();
             int get_maxalloc() { return maxalloc;}
